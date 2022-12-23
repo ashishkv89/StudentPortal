10c98d7e995f8ccae4d03e9d51cd297b242bdb89
Ashish KV <ashishkv89@gmail.com>
2022-12-23 20:14:38 +0000

    Users Controller index and show done

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,13 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('user_index')" :active="request()->routeIs('user_index')">
+                        {{ __('Users') }}
+                    </x-nav-link>
+                </div>mm, 
+
             </div>
 
             <!-- Settings Dropdown -->
