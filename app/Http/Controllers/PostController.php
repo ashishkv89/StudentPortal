241d944fe216fb99a3b524df1e7ca5fbd01ca875
Ashish KV <ashishkv89@gmail.com>
2023-01-02 17:39:22 +0000

    Final View enhancements before Notification Set Up

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderby('created_at', 'desc')->paginate(7);
+        $posts = Post::orderby('created_at', 'desc')->paginate(10);
         return view('posts.index', ['posts' => $posts]);
     }
 
